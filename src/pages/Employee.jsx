0e3b041f01242d8118564334a85ac9d7ec458c78
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-06-10 23:51:23 +0200

    admin dashboard

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import EmployeeTable from "../components/EmployeeTable";
 import SideBar from "../components/SideBar";
+import NavBar from "../components/NavBar";
 export default function Employee (){
     return(
         <div style={{gridTemplateColumns:"300px 1fr",gridTemplateRows:"80px 1fr"}} className='grid g h-screen '>
+            <NavBar />
             <SideBar />
             <EmployeeTable />
         </div>
