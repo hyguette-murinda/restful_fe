7a4d7d8969b7fc096fd4407fda53948c227ecdcf
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-06-10 21:54:27 +0200

    login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,70 +1,55 @@
 import React from 'react';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
-const LoginForm = () =>{
-    const onFinish = (values) => {
-        console.log('Received values of form: ', values);
-      };
-    return(
-        <div>
-            <div>
-            <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
 
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
+import { Link } from "react-router-dom";
+import loginIllustration from '../assets/login2.svg'
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
+const LoginForm = () => {
+  return <div className='bg-gray-100 h-screen flex items-center '>
+    <div style={{margin:"0 auto"}} className="bg-white w-[80%] h-[90%] rounded-lg flex">
+        <div className='max-w-96  h-full flex flex-col justify-center items-center '>
+            <div className="pr-8">
+            <h1 className='bold pb-4  text-[#101540] font-bold text-2xl'>Login</h1>
+            <p className='text-sm'>Welcome back please enter your details</p>
             </div>
-            <div>
+            <form action="" className=' p-8'>
+                <div className="flex flex-col text-sm p-4">
+                    <label htmlFor="email">Email*</label>
+                <input 
+                type="text"
+                name='email' 
+                placeholder='Enter your email'
+                className='p-2.5 w-full border border-gray-300 rounded-md bg-gray-100'
+                /> 
+                </div>
+              
+               <div className="flex flex-col text-sm p-4">
+                    <label htmlFor="password">Password*</label>
+                <input 
+                type="password"
+                name='password' 
+                placeholder='Minimum 8 characters'
+                className='p-2.5 border w-full border-gray-300 rounded-md bg-gray-100'
+                /> 
+               </div>
+               <div className="pt-2.5 flex p-4  justify-between ">
+                <input 
+                type="checkbox"
+                className='w-4'
 
-            </div>
+                 ></input>
+               <label className='text-[14px] pr-12' >Remember me</label> 
+               <a href="" className='text-[14px] text-[#101540] font-semibold '>Forgot Password?</a>
+               </div>
+
+            <button className='w-80 ml-2  bg-[#0B1355] font-medium rounded-lg text-sm   py-2.5 text-center text-white'>Login</button>
+            <p className='text-sm p-2 pt-4'>Don't have an account? 
+            <Link to="/register" className="font-medium text-primary-600 hover:underline">Sign up</Link></p>
+            </form>
         </div>
-    )
-}
-
-export default LoginForm
+        <div className=" w-full  flex items-center justify-center ">
+        <img className=' w-[80%]' src={loginIllustration} alt="Login Illustration" />
+        </div>
+    </div>
+  </div>;
+};
+export default LoginForm;
